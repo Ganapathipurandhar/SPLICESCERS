b6ccbae66e09281698a0a2abe96c49ffad3d0c06
Ashok Kumar Ulichi <ashok.ulichi@gmail.com>
2024-11-18 21:42:48 -0500

    Committing WorkServices related changes

--- SPLICESCERS/Work/WorkService.cs
+++ SPLICESCERS/Work/WorkService.cs
@@ -44,6 +44,46 @@ namespace SPLICESCERS.Work
 
                 Enum.TryParse(applicationSettings["TIER"], out Tiers ti);
                 _workData.Tier = ti;
+
+                _workData.EEContrBasic = Convert.ToDouble(applicationSettings["EEContBasic"]);
+                _workData.EEContrCol = Convert.ToDouble(applicationSettings["EEContCol"]);
+
+
+                Enum.TryParse(applicationSettings["MoneyPurchase"], out YesNo yesNo);
+                _workData.MoneyPurchase = yesNo;
+
+                _workData.MemberInfo = new PersonInfo();
+                _workData.MemberInfo.Name = applicationSettings["MemberName"];
+                _workData.MemberInfo.DOB = DateTime.Parse(applicationSettings["MemberDOB"]);
+
+                _workData.BeneficiaryInfo = new PersonInfo();
+                _workData.BeneficiaryInfo.Name = applicationSettings["BeneficiaryName"];
+                _workData.BeneficiaryInfo.DOB = DateTime.Parse(applicationSettings["BeneficiaryDOB"]);
+
+                _workData.RelationShip = applicationSettings["Relationship"];
+
+                _workData.IntegratedService = new DurationYMDs();
+                _workData.IntegratedService.Years = Convert.ToInt32(applicationSettings["ISY"]);
+                _workData.IntegratedService.Months = Convert.ToInt32(applicationSettings["ISM"]);
+                _workData.IntegratedService.Days = Convert.ToDouble(applicationSettings["ISD"]);
+
+                _workData.ISSick = new DurationYMDs();
+                _workData.ISSick.Years = Convert.ToInt32(applicationSettings["SLIY"]);
+                _workData.ISSick.Months = Convert.ToInt32(applicationSettings["SLIM"]);
+                _workData.ISSick.Days = Convert.ToDouble(applicationSettings["SLID"]);
+
+                _workData.NonIntegratedService = new DurationYMDs();
+                _workData.NonIntegratedService.Years = Convert.ToInt32(applicationSettings["NISY"]);
+                _workData.NonIntegratedService.Months = Convert.ToInt32(applicationSettings["NISM"]);
+                _workData.NonIntegratedService.Days = Convert.ToInt32(applicationSettings["NISD"]);
+
+                _workData.NonISSick = new DurationYMDs();
+                _workData.NonISSick.Years = Convert.ToInt32(applicationSettings["SLNIY"]);
+                _workData.NonISSick.Years = Convert.ToInt32(applicationSettings["SLNIM"]);
+                _workData.NonISSick.Years = Convert.ToInt32(applicationSettings["SLNID"]);
+
+                _workData.FinalComp = Convert.ToDouble(applicationSettings["FinalComp"]);
+
             }
 
 		}
